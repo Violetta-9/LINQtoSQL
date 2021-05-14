9acedf7ff7f48aa987c97726e8b2c68d34645a63
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2021-05-14 17:55:51 +0300

    Добавьте файлы проекта.

---
+++ LINQtoSQL/Players.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq.Mapping;
+
+namespace LINQtoSQL
+{
+    [Table(Name = "Players")]
+    class Players
+    {
+        [Column(Name="Id",IsPrimaryKey = true, IsDbGenerated = true)]//IsPrimaryKey: хранит логическое значение и указывает, выполняет ли столбец роль первичного ключа (как в данном случае Id)
+        public int PlayerId { get; set; }
+        [Column]
+        public string Name { get; set; }
+        [Column(Name = "Position")]//явно прописываем, что берем колонку с именем Name
+        public string Position { get; set; }
+        [Column]//если же имя колонки совпадает с именем свойства, то можно написать так
+        public int Age { get; set; }
+
+
+    }
+}
---
+++ LINQtoSQL/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+
+namespace LINQtoSQL
+{
+    class Program
+    {
+        static string connectionString = @"Data Source=DESKTOP-NVEEOK3\SQLEXPRESS;Initial Catalog=Football;Integrated Security=True";
+        static void Main(string[] args)
+        {
+            DataContext db = new DataContext(connectionString);//Сначала создается контекст данных, который представлен объектом DataContext.В конструктор этого класса передается строка подключения.Через контекст данных мы будем работать с базой данных.
+
+           // Получаем таблицу пользователей
+           Table<Players> players = db.GetTable<Players>();
+            var plauersswap = from item in players
+                              where item.Age > 18
+                              orderby item.Name
+                              select item;
+            var plauersSwap=players.Where()
+            foreach (var item in plauersswap)
+            {
+                Console.WriteLine("{0} \t{1} \t{2} \t{3}",item.PlayerId, item.Name,item.Position, item.Age);
+            }
+
+        }
+    }
+}
