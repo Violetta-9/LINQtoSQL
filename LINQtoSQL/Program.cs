7fc3eeb9664b730e34cf05262e48d46d5f9e2e30
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2021-05-16 11:40:20 +0300

    use InsertOnSubmit,InsertAllOnSubmit,try,catch

--- LINQtoSQL/Program.cs
+++ LINQtoSQL/Program.cs
@@ -54,11 +54,61 @@ namespace LINQtoSQL
             var players1=players.FirstOrDefault();
             players1.Name = "Алеся";
             db.SubmitChanges();
+
+            Console.WriteLine();
+
+            foreach (var item in players)
+            {
+                Console.WriteLine("{0} \t{1} \t{2}", item.PlayerId, item.Name, item.Age);
+            }
+            //var playerNew = new Players { Name = "Антон", Age = 25, Position = "Нападающий" };
+            //players.InsertOnSubmit(playerNew);
+            //db.SubmitChanges();
+            //Console.WriteLine();
+            //foreach (var item in players)
+            //{
+            //    Console.WriteLine("{0} \t{1} \t{2}", item.PlayerId, item.Name, item.Age);
+            //}
+            //Console.WriteLine();
+            //var playerList = new List<Players>()
+            //{
+            //    new Players{ Name="Vasj",Age=34,Position="Нападающий"},
+            //    new Players{ Name="Таня",Age=34,Position="Вротарь"},
+            //    new Players{ Name="Маша",Age=34,Position="Главный"}
+
+            //};
+            //players.InsertAllOnSubmit(playerList);
+            //db.SubmitChanges();
+            //foreach (var item in players)
+            //{
+            //    Console.WriteLine("{0} \t{1} \t{2}", item.PlayerId, item.Name, item.Age);
+            //}
+            var playerDelete = players.FirstOrDefault(item=>item.Name=="Антон ");
+            try
+            {
+                players.DeleteOnSubmit(playerDelete);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+               
+            }
+            db.SubmitChanges();
             Console.WriteLine();
             foreach (var item in players)
             {
                 Console.WriteLine("{0} \t{1} \t{2}", item.PlayerId, item.Name, item.Age);
             }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //players.DeleteAllOnSubmit(players);
+            //db.SubmitChanges();
+            //Console.WriteLine("Вывод");
+            //foreach (var item in players)
+            //{
+            //    Console.WriteLine("{0} \t{1} \t{2}", item.PlayerId, item.Name, item.Age);
+            //}
 
         }
     }
